e5e91053965109dde144a90113edb80a916dee7a
gengteng <me@gteng.org>
2025-01-09 18:46:37 +0800

    init

---
+++ src/src/account.rs
@@ -0,0 +1,189 @@
+use anchor_lang::prelude::*;
+use anchor_lang::prelude::*;
+use std::ops::{BitAnd, BitOr, BitXor};
+/// Seed to derive account address and signature
+pub const POOL_SEED: &str = "pool";
+pub const POOL_LP_MINT_SEED: &str = "pool_lp_mint";
+pub const POOL_VAULT_SEED: &str = "pool_vault";
+
+pub const Q32: u128 = (u32::MAX as u128) + 1; // 2^32
+
+pub const AMM_CONFIG_SEED: &str = "amm_config";
+
+/// Holds the current owner of the factory
+#[account]
+#[derive(Default, Debug)]
+pub struct AmmConfig {
+    /// Bump to identify PDA
+    pub bump: u8,
+    /// Status to control if new pool can be create
+    pub disable_create_pool: bool,
+    /// Config index
+    pub index: u16,
+    /// The trade fee, denominated in hundredths of a bip (10^-6)
+    pub trade_fee_rate: u64,
+    /// The protocol fee
+    pub protocol_fee_rate: u64,
+    /// The fund fee, denominated in hundredths of a bip (10^-6)
+    pub fund_fee_rate: u64,
+    /// Fee for create a new pool
+    pub create_pool_fee: u64,
+    /// Address of the protocol fee owner
+    pub protocol_owner: Pubkey,
+    /// Address of the fund fee owner
+    pub fund_owner: Pubkey,
+    /// padding
+    pub padding: [u64; 16],
+}
+
+impl AmmConfig {
+    pub const LEN: usize = 8 + 1 + 1 + 2 + 4 * 8 + 32 * 2 + 8 * 16;
+}
+
+pub enum PoolStatusBitIndex {
+    Deposit,
+    Withdraw,
+    Swap,
+}
+
+#[derive(PartialEq, Eq)]
+pub enum PoolStatusBitFlag {
+    Enable,
+    Disable,
+}
+
+#[account(zero_copy(unsafe))]
+#[repr(packed)]
+#[derive(Default, Debug)]
+pub struct PoolState {
+    /// Which config the pool belongs
+    pub amm_config: Pubkey,
+    /// pool creator
+    pub pool_creator: Pubkey,
+    /// Token A
+    pub token_0_vault: Pubkey,
+    /// Token B
+    pub token_1_vault: Pubkey,
+
+    /// Pool tokens are issued when A or B tokens are deposited.
+    /// Pool tokens can be withdrawn back to the original A or B token.
+    pub lp_mint: Pubkey,
+    /// Mint information for token A
+    pub token_0_mint: Pubkey,
+    /// Mint information for token B
+    pub token_1_mint: Pubkey,
+
+    /// token_0 program
+    pub token_0_program: Pubkey,
+    /// token_1 program
+    pub token_1_program: Pubkey,
+
+    /// observation account to store oracle data
+    pub observation_key: Pubkey,
+
+    pub auth_bump: u8,
+    /// Bitwise representation of the state of the pool
+    /// bit0, 1: disable deposit(vaule is 1), 0: normal
+    /// bit1, 1: disable withdraw(vaule is 2), 0: normal
+    /// bit2, 1: disable swap(vaule is 4), 0: normal
+    pub status: u8,
+
+    pub lp_mint_decimals: u8,
+    /// mint0 and mint1 decimals
+    pub mint_0_decimals: u8,
+    pub mint_1_decimals: u8,
+
+    /// True circulating supply without burns and lock ups
+    pub lp_supply: u64,
+    /// The amounts of token_0 and token_1 that are owed to the liquidity provider.
+    pub protocol_fees_token_0: u64,
+    pub protocol_fees_token_1: u64,
+
+    pub fund_fees_token_0: u64,
+    pub fund_fees_token_1: u64,
+
+    /// The timestamp allowed for swap in the pool.
+    pub open_time: u64,
+    /// recent epoch
+    pub recent_epoch: u64,
+    /// padding for future updates
+    pub padding: [u64; 31],
+}
+
+impl PoolState {
+    pub const LEN: usize = 8 + 10 * 32 + 1 * 5 + 8 * 7 + 8 * 31;
+
+    /// Get status by bit, if it is `noraml` status, return true
+    pub fn get_status_by_bit(&self, bit: PoolStatusBitIndex) -> bool {
+        let status = u8::from(1) << (bit as u8);
+        self.status.bitand(status) == 0
+    }
+
+    pub fn vault_amount_without_fee(&self, vault_0: u64, vault_1: u64) -> (u64, u64) {
+        (
+            vault_0
+                .checked_sub(self.protocol_fees_token_0 + self.fund_fees_token_0)
+                .unwrap(),
+            vault_1
+                .checked_sub(self.protocol_fees_token_1 + self.fund_fees_token_1)
+                .unwrap(),
+        )
+    }
+
+    pub fn token_price_x32(&self, vault_0: u64, vault_1: u64) -> (u128, u128) {
+        let (token_0_amount, token_1_amount) = self.vault_amount_without_fee(vault_0, vault_1);
+        (
+            token_1_amount as u128 * Q32 as u128 / token_0_amount as u128,
+            token_0_amount as u128 * Q32 as u128 / token_1_amount as u128,
+        )
+    }
+}
+
+/// Seed to derive account address and signature
+pub const OBSERVATION_SEED: &str = "observation";
+// Number of ObservationState element
+pub const OBSERVATION_NUM: usize = 100;
+pub const OBSERVATION_UPDATE_DURATION_DEFAULT: u64 = 15;
+
+/// The element of observations in ObservationState
+#[zero_copy(unsafe)]
+#[repr(packed)]
+#[derive(Default, Debug)]
+pub struct Observation {
+    /// The block timestamp of the observation
+    pub block_timestamp: u64,
+    /// the cumulative of token0 price during the duration time, Q32.32, the remaining 64 bit for overflow
+    pub cumulative_token_0_price_x32: u128,
+    /// the cumulative of token1 price during the duration time, Q32.32, the remaining 64 bit for overflow
+    pub cumulative_token_1_price_x32: u128,
+}
+impl Observation {
+    pub const LEN: usize = 8 + 16 + 16;
+}
+
+#[account(zero_copy(unsafe))]
+#[repr(packed)]
+pub struct ObservationState {
+    /// Whether the ObservationState is initialized
+    pub initialized: bool,
+    /// the most-recently updated index of the observations array
+    pub observation_index: u16,
+    pub pool_id: Pubkey,
+    /// observation array
+    pub observations: [Observation; OBSERVATION_NUM],
+    /// padding for feature update
+    pub padding: [u64; 4],
+}
+
+impl Default for ObservationState {
+    #[inline]
+    fn default() -> ObservationState {
+        ObservationState {
+            initialized: false,
+            observation_index: 0,
+            pool_id: Pubkey::default(),
+            observations: [Observation::default(); OBSERVATION_NUM],
+            padding: [0u64; 4],
+        }
+    }
+}
---
+++ src/src/context.rs
@@ -0,0 +1,358 @@
+use anchor_lang::prelude::*;
+
+use crate::{AmmConfig, ObservationState, PoolState};
+
+#[account]
+pub struct AccountPlaceholder {}
+
+#[derive(Accounts)]
+#[instruction(index: u16)]
+pub struct CreateAmmConfig<'info> {
+    /// Address to be set as protocol owner.
+    #[account(mut)]
+    pub owner: Signer<'info>,
+
+    /// Initialize config state account to store protocol owner address and fee rates.
+    #[account(mut)]
+    pub amm_config: Account<'info, AmmConfig>,
+
+    pub system_program: Account<'info, AccountPlaceholder>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateAmmConfig<'info> {
+    /// The amm config owner or admin
+    pub owner: Signer<'info>,
+
+    /// Amm config account to be changed
+    #[account(mut)]
+    pub amm_config: Account<'info, AmmConfig>,
+}
+
+#[derive(Accounts)]
+pub struct UpdatePoolStatus<'info> {
+    pub authority: Signer<'info>,
+
+    #[account(mut)]
+    pub pool_state: AccountLoader<'info, PoolState>,
+}
+
+#[derive(Accounts)]
+pub struct CollectProtocolFee<'info> {
+    /// Only admin or owner can collect fee now
+    pub owner: Signer<'info>,
+
+    /// CHECK: pool vault and lp mint authority
+    pub authority: Account<'info, AccountPlaceholder>,
+
+    /// Pool state stores accumulated protocol fee amount
+    #[account(mut)]
+    pub pool_state: AccountLoader<'info, PoolState>,
+
+    /// Amm config account stores owner
+    pub amm_config: Account<'info, AmmConfig>,
+
+    /// The address that holds pool tokens for token_0
+    #[account(mut)]
+    pub token_0_vault: Account<'info, AccountPlaceholder>,
+
+    /// The address that holds pool tokens for token_1
+    #[account(mut)]
+    pub token_1_vault: Account<'info, AccountPlaceholder>,
+
+    /// The mint of token_0 vault
+    #[account(mut)]
+    pub vault_0_mint: Account<'info, AccountPlaceholder>,
+
+    /// The mint of token_1 vault
+    #[account(mut)]
+    pub vault_1_mint: Account<'info, AccountPlaceholder>,
+
+    /// The address that receives the collected token_0 protocol fees
+    #[account(mut)]
+    pub recipient_token_0_account: Account<'info, AccountPlaceholder>,
+
+    /// The address that receives the collected token_1 protocol fees
+    #[account(mut)]
+    pub recipient_token_1_account: Account<'info, AccountPlaceholder>,
+
+    /// The SPL program to perform token transfers
+    pub token_program: Account<'info, AccountPlaceholder>,
+
+    /// The SPL program 2022 to perform token transfers
+    pub token_program_2022: Account<'info, AccountPlaceholder>,
+}
+
+#[derive(Accounts)]
+pub struct CollectFundFee<'info> {
+    /// Only admin or fund_owner can collect fee now
+    pub owner: Signer<'info>,
+
+    /// CHECK: pool vault and lp mint authority
+    pub authority: Account<'info, AccountPlaceholder>,
+
+    /// Pool state stores accumulated protocol fee amount
+    #[account(mut)]
+    pub pool_state: AccountLoader<'info, PoolState>,
+
+    /// Amm config account stores fund_owner
+    pub amm_config: Account<'info, AmmConfig>,
+
+    /// The address that holds pool tokens for token_0
+    #[account(mut)]
+    pub token_0_vault: Account<'info, AccountPlaceholder>,
+
+    /// The address that holds pool tokens for token_1
+    #[account(mut)]
+    pub token_1_vault: Account<'info, AccountPlaceholder>,
+
+    /// The mint of token_0 vault
+    #[account(mut)]
+    pub vault_0_mint: Account<'info, AccountPlaceholder>,
+
+    /// The mint of token_1 vault
+    #[account(mut)]
+    pub vault_1_mint: Account<'info, AccountPlaceholder>,
+
+    /// The address that receives the collected token_0 fund fees
+    #[account(mut)]
+    pub recipient_token_0_account: Account<'info, AccountPlaceholder>,
+
+    /// The address that receives the collected token_1 fund fees
+    #[account(mut)]
+    pub recipient_token_1_account: Account<'info, AccountPlaceholder>,
+
+    /// The SPL program to perform token transfers
+    pub token_program: Account<'info, AccountPlaceholder>,
+
+    /// The SPL program 2022 to perform token transfers
+    pub token_program_2022: Account<'info, AccountPlaceholder>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    /// Address paying to create the pool. Can be anyone
+    #[account(mut)]
+    pub creator: Signer<'info>,
+
+    /// Which config the pool belongs to.
+    pub amm_config: Account<'info, AmmConfig>,
+
+    /// CHECK: pool vault and lp mint authority
+    pub swap_authority: Account<'info, AccountPlaceholder>,
+
+    /// CHECK: Initialize an account to store the pool state
+    #[account(mut)]
+    pub pool_state: AccountLoader<'info, PoolState>,
+
+    /// Token_0 mint, the key must smaller then token_1 mint.
+    pub token_0_mint: Account<'info, AccountPlaceholder>,
+
+    /// Token_1 mint, the key must greater than token_0 mint.
+    pub token_1_mint: Account<'info, AccountPlaceholder>,
+
+    /// pool lp mint
+    #[account(mut)]
+    pub lp_mint: Account<'info, AccountPlaceholder>,
+
+    /// payer token0 account
+    #[account(mut)]
+    pub creator_token_0: Account<'info, AccountPlaceholder>,
+
+    /// creator token1 account
+    #[account(mut)]
+    pub creator_token_1: Account<'info, AccountPlaceholder>,
+
+    /// creator lp token account
+    #[account(mut)]
+    pub creator_lp_token: Account<'info, AccountPlaceholder>,
+
+    /// CHECK: Token_0 vault for the pool, created by contract
+    #[account(mut)]
+    pub token_0_vault: Account<'info, AccountPlaceholder>,
+
+    /// CHECK: Token_1 vault for the pool, created by contract
+    #[account(mut)]
+    pub token_1_vault: Account<'info, AccountPlaceholder>,
+
+    /// create pool fee account
+    #[account(mut)]
+    pub create_pool_fee: Account<'info, AccountPlaceholder>,
+
+    /// an account to store oracle observations
+    #[account(mut)]
+    pub observation_state: AccountLoader<'info, ObservationState>,
+
+    /// Program to create mint account and mint tokens
+    pub token_program: Account<'info, AccountPlaceholder>,
+
+    /// Spl token program or token program 2022
+    pub token_0_program: Account<'info, AccountPlaceholder>,
+
+    /// Spl token program or token program 2022
+    pub token_1_program: Account<'info, AccountPlaceholder>,
+
+    /// Program to create an ATA for receiving position NFT
+    pub associated_token_program: Account<'info, AccountPlaceholder>,
+
+    /// To create a new program account
+    pub system_program: Account<'info, AccountPlaceholder>,
+
+    /// Sysvar for program account
+    pub rent: Account<'info, AccountPlaceholder>,
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    /// Pays to mint the position
+    pub owner: Signer<'info>,
+
+    /// CHECK: pool vault and lp mint authority
+    pub authority: Account<'info, AccountPlaceholder>,
+
+    #[account(mut)]
+    pub pool_state: AccountLoader<'info, PoolState>,
+
+    /// Owner lp token account
+    #[account(mut)]
+    pub owner_lp_token: Account<'info, AccountPlaceholder>,
+
+    /// The payer's token account for token_0
+    #[account(mut)]
+    pub token_0_account: Account<'info, AccountPlaceholder>,
+
+    /// The payer's token account for token_1
+    #[account(mut)]
+    pub token_1_account: Account<'info, AccountPlaceholder>,
+
+    /// The address that holds pool tokens for token_0
+    #[account(mut)]
+    pub token_0_vault: Account<'info, AccountPlaceholder>,
+
+    /// The address that holds pool tokens for token_1
+    #[account(mut)]
+    pub token_1_vault: Account<'info, AccountPlaceholder>,
+
+    /// Token Program
+    pub token_program: Account<'info, AccountPlaceholder>,
+
+    /// Token program 2022
+    pub token_program_2022: Account<'info, AccountPlaceholder>,
+
+    /// The mint of token_0 vault
+    #[account(mut)]
+    pub vault_0_mint: Account<'info, AccountPlaceholder>,
+
+    /// The mint of token_1 vault
+    #[account(mut)]
+    pub vault_1_mint: Account<'info, AccountPlaceholder>,
+
+    /// Lp token mint
+    #[account(mut)]
+    pub lp_mint: Account<'info, AccountPlaceholder>,
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    /// Pays to mint the position
+    pub owner: Signer<'info>,
+
+    /// CHECK: pool vault and lp mint authority
+    pub authority: Account<'info, AccountPlaceholder>,
+
+    /// Pool state account
+    #[account(mut)]
+    pub pool_state: AccountLoader<'info, PoolState>,
+
+    /// Owner lp token account
+    #[account(mut)]
+    pub owner_lp_token: Account<'info, AccountPlaceholder>,
+
+    /// The token account for receive token_0
+    #[account(mut)]
+    pub token_0_account: Account<'info, AccountPlaceholder>,
+
+    /// The token account for receive token_1
+    #[account(mut)]
+    pub token_1_account: Account<'info, AccountPlaceholder>,
+
+    /// The address that holds pool tokens for token_0
+    #[account(mut)]
+    pub token_0_vault: Account<'info, AccountPlaceholder>,
+
+    /// The address that holds pool tokens for token_1
+    #[account(mut)]
+    pub token_1_vault: Account<'info, AccountPlaceholder>,
+
+    /// Token Program
+    pub token_program: Account<'info, AccountPlaceholder>,
+
+    /// Token program 2022
+    pub token_program_2022: Account<'info, AccountPlaceholder>,
+
+    /// The mint of token_0 vault
+    #[account(mut)]
+    pub vault_0_mint: Account<'info, AccountPlaceholder>,
+
+    /// The mint of token_1 vault
+    #[account(mut)]
+    pub vault_1_mint: Account<'info, AccountPlaceholder>,
+
+    /// Pool lp token mint
+    #[account(mut)]
+    pub lp_mint: Account<'info, AccountPlaceholder>,
+
+    /// memo program
+    /// CHECK:
+    pub memo_program: Account<'info, AccountPlaceholder>,
+}
+
+#[derive(Accounts)]
+pub struct Swap<'info> {
+    /// The user performing the swap
+    pub payer: Signer<'info>,
+
+    /// CHECK: pool vault and lp mint authority
+    pub authority: Account<'info, AccountPlaceholder>,
+
+    /// The factory state to read protocol fees
+    pub amm_config: Account<'info, AmmConfig>,
+
+    /// The program account of the pool in which the swap will be performed
+    #[account(mut)]
+    pub pool_state: AccountLoader<'info, PoolState>,
+
+    /// The user token account for input token
+    #[account(mut)]
+    pub input_token_account: Account<'info, AccountPlaceholder>,
+
+    /// The user token account for output token
+    #[account(mut)]
+    pub output_token_account: Account<'info, AccountPlaceholder>,
+
+    /// The vault token account for input token
+    #[account(mut)]
+    pub input_vault: Account<'info, AccountPlaceholder>,
+
+    /// The vault token account for output token
+    #[account(mut)]
+    pub output_vault: Account<'info, AccountPlaceholder>,
+
+    /// SPL program for input token transfers
+    pub input_token_program: Account<'info, AccountPlaceholder>,
+
+    /// SPL program for output token transfers
+    pub output_token_program: Account<'info, AccountPlaceholder>,
+
+    /// The mint of input token
+    #[account(mut)]
+    pub input_token_mint: Account<'info, AccountPlaceholder>,
+
+    /// The mint of output token
+    #[account(mut)]
+    pub output_token_mint: Account<'info, AccountPlaceholder>,
+
+    /// The program account for the most recent oracle observation
+    #[account(mut)]
+    pub observation_state: AccountLoader<'info, ObservationState>,
+}
